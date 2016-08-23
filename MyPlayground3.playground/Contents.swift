2531cf97cc75d86bf2c3b383260b45cc6bb109db
vitaliy.manov <manu1991@yandex.ru>
2016-08-23 20:10:28 +0300

    add fail playground

---
+++ MyPlayground3.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let range = 0...100
+
+for i in range {
+    if i % 3 == 0 && i % 2 != = && 0 %5 != {
+        print(i)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
